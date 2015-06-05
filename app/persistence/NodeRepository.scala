e94f2934abaa2a2f2cdd507da28fe8887db289cd
Martin Helmich <kontakt@martin-helmich.de>
2015-06-05 17:13:14 +0200

    Add option to match nodes by query

--- app/persistence/NodeRepository.scala
+++ app/persistence/NodeRepository.scala
@@ -4,13 +4,28 @@ import org.neo4j.graphdb.Node
 
 class NodeRepository(backend: BackendInterface) {
 
-  def merge(q: Query): Node = {
-    val propertyFilter = q.properties match {
-      case null => ""
-      case map: Map[String, Any] => map map { case (key, _) => s"$key: {props}.$key" } mkString ", "
+  def find(q: Query): Option[Node] = {
+    val pf = propertyFilter(q)
+
+    val cypher = s"""MATCH (n:${q.label.name} {$pf}) RETURN n LIMIT 1"""
+    val result = q.properties match {
+      case null => backend.execute(cypher).run()
+      case m: Map[String, Any] => backend.execute(cypher).runWith(Map("props" -> q.properties))
     }
 
-    val cypher = s"""MERGE (n:${q.label.name} {$propertyFilter}) RETURN n"""
+    if (result.hasNext) {
+      val node = result.columnAs[Node]("n").next()
+      result.close()
+      Some(node)
+    } else {
+      None
+    }
+  }
+
+  def merge(q: Query): Node = {
+    val pf = propertyFilter(q)
+
+    val cypher = s"""MERGE (n:${q.label.name} {$pf}) RETURN n"""
     val result = q.properties match {
       case null => backend.execute(cypher).run()
       case m: Map[String, Any] => backend.execute(cypher).runWith(Map("props" -> q.properties))
@@ -19,6 +34,11 @@ class NodeRepository(backend: BackendInterface) {
     val node = result.columnAs[Node]("n").next()
     result.close()
     return node
+  }
+
+  protected def propertyFilter(q: Query) = q.properties match {
+    case null => ""
+    case map: Map[String, Any] => map map { case (key, _) => s"$key: {props}.$key" } mkString ", "
   }
 
 }
