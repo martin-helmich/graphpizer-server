f9e4d283625344b43177f24b49190e4cc9833e31
Martin Helmich <kontakt@martin-helmich.de>
2015-06-05 17:13:29 +0200

    Add file resources (useful for caching)

--- app/controllers/Files.scala
+++ app/controllers/Files.scala
@@ -3,31 +3,58 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import org.neo4j.graphdb.Node
-import persistence.ConnectionManager
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import persistence.{Query, ConnectionManager}
+import persistence.NodeWrappers._
 import play.api.libs.json.{JsArray, Json}
 import play.api.mvc._
-
-import scala.collection.JavaConversions._
-import scala.concurrent.Future
+import domain.model.AstLabelType._
 
 @Singleton
 class Files @Inject()(manager: ConnectionManager) extends Controller {
 
+  val FileNotFound = (p: String) => NotFound(Json.obj("status" -> "ko", "messsage" -> s"File '$p' does not exist"))
+
   def list(project: String) = Action {
     val json = manager connect project transactional { (b, t) =>
-      val result = b execute "MATCH (p:File) RETURN p" run
-      val nodes: Iterator[Node] = result.columnAs[Node]("p")
-
       JsArray(
-        nodes.toArray.map({ n =>
+        b execute "MATCH (p:File) RETURN p" map { (file: Node) =>
           Json.obj(
-            "filename" -> (n getProperty "filename").asInstanceOf[String]
+            "filename" -> file.property[String]("filename"),
+            "checksum" -> file.property[String]("checksum")
           )
-        })
+        }
       )
     }
     Ok(json)
+  }
+
+  def show(project: String, path: String) = Action { request =>
+    manager connect project transactional { (b, _) =>
+      val q = new Query(File, Map("filename" -> path))
+      b.nodes find q match {
+        case Some(file) =>
+          val json = Json.obj(
+            "filename" -> file.property[String]("filename"),
+            "checksum" -> file.property[String]("checksum")
+          )
+          Ok(json)
+        case None => FileNotFound(path)
+      }
+    }
+  }
+
+  def check(project: String, path: String) = Action { request =>
+    val checksum = request.headers.get("etag")
+
+    manager connect project transactional { (b, _) =>
+      val q = new Query(File, Map("filename" -> path))
+      b.nodes find q match {
+        case Some(file) =>
+          if (file.property[String]("checksum") == checksum) {NotModified }
+          else {Ok }
+        case None => FileNotFound(path)
+      }
+    }
   }
 
 }
